39e67ba4a9e451dbc879324d89602c58927dc06a
MalfinK <muhammadalfin1235678@gmail.com>
2023-12-21 21:44:57 +0700

    Vid pertama done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.belajarcompose"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.belajarcompose"
